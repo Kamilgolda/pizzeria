3d9108c6fd496d46ff49cf5f4c4d2332e2bf94f0
Kamilgolda <50715733+Kamilgolda@users.noreply.github.com>
2019-05-18 18:45:26 +0200

    zxc

--- Pizzeria/Pizzeria/Projekt.xaml.cs
+++ Pizzeria/Pizzeria/Projekt.xaml.cs
@@ -24,5 +24,13 @@ namespace Pizzeria
         {
             InitializeComponent();
         }
+<<<<<<< HEAD
+=======
+
+        private void Button_nasze_pizze_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+>>>>>>> parent of 1bcd8c4... dodanie materialu
     }
 }
